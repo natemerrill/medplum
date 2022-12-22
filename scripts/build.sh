b4f42d2afa4acea77fd256a8d41657d872439464
Nathan Merrill <nmerrill5@gmail.com>
2022-12-22 13:35:01 -0700

    No test in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,21 +19,21 @@ npm --version
 npx turbo run build
 
 # Test
-npx turbo run test -- --coverage
+# npx turbo run test -- --coverage
 
 # Combine test coverage
-rm -rf coverage
-mkdir -p coverage/packages
-mkdir -p coverage/combined
-cp packages/app/coverage/coverage-final.json coverage/packages/coverage-app.json
-cp packages/core/coverage/coverage-final.json coverage/packages/coverage-core.json
-cp packages/cli/coverage/coverage-final.json coverage/packages/coverage-cli.json
-cp packages/infra/coverage/coverage-final.json coverage/packages/coverage-infra.json
-cp packages/mock/coverage/coverage-final.json coverage/packages/coverage-mock.json
-cp packages/react/coverage/coverage-final.json coverage/packages/coverage-react.json
-cp packages/server/coverage/coverage-final.json coverage/packages/coverage-server.json
-npx nyc merge coverage/packages coverage/combined/coverage.json
-npx nyc report -t coverage/combined --report-dir coverage --reporter=lcov
+# rm -rf coverage
+# mkdir -p coverage/packages
+# mkdir -p coverage/combined
+# cp packages/app/coverage/coverage-final.json coverage/packages/coverage-app.json
+# cp packages/core/coverage/coverage-final.json coverage/packages/coverage-core.json
+# cp packages/cli/coverage/coverage-final.json coverage/packages/coverage-cli.json
+# cp packages/infra/coverage/coverage-final.json coverage/packages/coverage-infra.json
+# cp packages/mock/coverage/coverage-final.json coverage/packages/coverage-mock.json
+# cp packages/react/coverage/coverage-final.json coverage/packages/coverage-react.json
+# cp packages/server/coverage/coverage-final.json coverage/packages/coverage-server.json
+# npx nyc merge coverage/packages coverage/combined/coverage.json
+# npx nyc report -t coverage/combined --report-dir coverage --reporter=lcov
 
 # Lint
-npm run lint
+# npm run lint
